6af39bf73f0cfde55fce99723e1043c2c2da5474
gale <mgastonportillo@gmail.com>
2022-08-06 21:41:08 -0300

    version 1

---
+++ 01-cypher/my_script.js
@@ -0,0 +1,24 @@
+// Main block
+const encryptButton = document.getElementById("encrypt");
+const decryptButton = document.getElementById("decrypt");
+const scrollUp = document.getElementById("scrollUp");
+
+function encrypt() {
+    const textToEncrypt = document.getElementById("inputArea").value;
+}
+
+function decrypt() {
+    const textToEncrypt = document.getElementById("inputArea").value;
+}
+
+function scrollUp() {
+    scrollUp.onClick()
+}
+
+function newEncrypt() {
+    const textToEncrypt = document.getElementById("inputArea").value;
+}
+
+function newDecrypt() {
+    const textToEncrypt = document.getElementById("inputArea").value;
+}
