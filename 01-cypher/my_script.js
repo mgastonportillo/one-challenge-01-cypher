502b9d194a769de1734702d84916f910ad02e32b
gale <mgastonportillo@gmail.com>
2022-08-08 15:57:16 -0300

    v5

--- 01-cypher/my_script.js
+++ 01-cypher/my_script.js
@@ -30,6 +30,21 @@ function adjustHeight() {
     }
 }
 
+// Fix mobile screen issues
+function calculateVh() {
+    var vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+// Initial calculation
+calculateVh();
+
+// Re-calculate on resize
+window.addEventListener("resize", calculateVh);
+
+// Re-calculate on device orientation change
+window.addEventListener("orientationchange", calculateVh);
+
 // Copy to clipboard
 function confirmCopy(msgCopied) {
     const copyExists = document.querySelector(".copied");
